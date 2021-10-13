3f216d832b26da4bf81d38b9d451041849ca9ad7
amal <amal.141726@gmail.com>
2021-10-13 23:25:03 +0300

    finish

---
+++ proj w/taskTabelViewcell.swift
@@ -0,0 +1,30 @@
+//
+//  cell.swift
+//  proj w
+//
+//  Created by Amal on 07/03/1443 AH.
+//
+
+import UIKit
+
+protocol checkButtondelegate {
+   func checkTaskTapped(at index:IndexPath)
+}
+class taskTabelViewcell: UITableViewCell, UITextFieldDelegate {
+    var delegate:checkButtondelegate!
+    var indexpath: IndexPath!
+   func setup() {
+     self.cellfield.delegate = self
+  }
+   @IBOutlet weak var checkButton: UIButton!
+   @IBOutlet weak var cellfield: UITextField!
+   @IBAction func action(_ sender: Any) {
+       delegate.checkTaskTapped(at: indexpath)
+  }
+   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       let update = Task(taskTitle: cellfield.text, isCompleted: taskArray[indexpath.row].isCompleted)
+           taskArray[indexpath.row] = update
+           cellfield.resignFirstResponder()
+       return true    
+  }
+}
