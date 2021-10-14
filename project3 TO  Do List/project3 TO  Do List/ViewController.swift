6a4adc27a613ea52ee7fb85956a01affe0f9296a
tasnim <tasneemjafsher@gmail.com>
2021-10-14 13:24:31 +0300

    done

---
+++ project3 TO  Do List/project3 TO  Do List/ViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  project3 TO  Do List
+//
+//  Created by Tsnim Alqahtani on 08/03/1443 AH.
+//
+
+import UIKit
+
+
+struct Task {
+    var taskTitle: String?
+    var isCompleted = false
+}
+var taskArray = [Task]()
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, checkButtondelegate {
+
+    
+   
+    @IBOutlet weak var taskTitle: UITextField!
+   
+    @IBOutlet weak var tableView: UITableView!
+   
+    @IBOutlet weak var buttonPressed: UIButton!
+    
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if taskArray.count > 0 {
+            return taskArray.count
+        }else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as! taskTableViewCell
+        
+        cell.delegate = self
+        cell.indexpath = indexPath
+        cell.cellfield.text = taskArray[indexPath.row].taskTitle
+        cell.setup()
+      
+        
+        if taskArray[indexPath.row].isCompleted {
+            cell.checkButton.setImage(
+                UIImage(systemName: "checkmark.circle.fill"),
+                    for: .normal
+                )
+        }else{
+            cell.checkButton.setImage(
+                UIImage(systemName: "plus.app.fill"),
+                for: .normal
+            )
+        }
+        return cell
+    }
+    func checkTaskTapped(at index: IndexPath) {
+        if taskArray[index.row].isCompleted {
+            taskArray[index.row].isCompleted = false
+        }else{
+            taskArray[index.row].isCompleted = true
+        }
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            taskArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath] , with: .fade)
+        }
+        tableView.reloadData()
+    }
+    
+    @IBAction func action(_ sender: Any) {
+        
+        if taskTitle.hasText {
+            let newTask = Task(taskTitle: taskTitle.text, isCompleted: false)
+            taskArray.insert(newTask, at: 0)
+            tableView.reloadData()
+            
+            taskTitle.text = ""
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        
+    }
+}
+
---
+++ project3 TO  Do List/project3 TO  Do List/taskTableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  taskTableViewCell.swift
+//  project3 TO  Do List
+//
+//  Created by Tsnim Alqahtani on 08/03/1443 AH.
+//
+
+import UIKit
+
+protocol checkButtondelegate {
+    func checkTaskTapped(at index: IndexPath)
+}
+
+
+
+class taskTableViewCell: UITableViewCell, UITextFieldDelegate {
+    var delegate : checkButtondelegate!
+    var indexpath: IndexPath!
+    
+    func setup()  {
+        self.cellfield.delegate = self
+    }
+    
+    @IBOutlet weak var cellfield: UITextField!
+    @IBOutlet weak var checkButton: UIButton!
+    @IBAction func action(_ sender: Any) {
+        delegate.checkTaskTapped(at: indexpath)
+    }
+    
+    func textFieldShouldReturn(_ TextField: UITextField) -> Bool {
+        let update = Task(taskTitle: cellfield.text, isCompleted: taskArray[indexpath.row].isCompleted)
+        taskArray[indexpath.row] = update
+            cellfield.resignFirstResponder()
+            return true
+    }
+    
+        }
+
+
